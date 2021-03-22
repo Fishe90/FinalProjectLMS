faf2a31c315c657f41a2e9c897c6780242b26578
Fishe90 <dillon.f617@gmail.com>
2021-03-22 11:38:25 -0500

    Initial Commit

---
+++ FinalProjectLMS.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProjectLMS.UI.MVC.MvcApplication" Language="C#" %>
---
+++ FinalProjectLMS.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjectLMS.UI.MVC.Startup))]
+namespace FinalProjectLMS.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
