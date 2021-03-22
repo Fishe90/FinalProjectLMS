c6b85dfa96584474ff35530c852165dc6e9649e3
Fishe90 <dillon.f617@gmail.com>
2021-03-22 13:27:17 -0500

    Added DATA Layer + Scaffolded Basic Controllers w/ Views

---
+++ FinalProjectLMS.DATA.EF/Metadata/DBFinalProjectLMSMetadata.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectLMS.DATA.EF/*.Metadata*/
+{
+    #region CourseCompletions
+    public class CourseCompletionsMetadata
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        [StringLength(128, ErrorMessage = "*User ID must not exceed 128 characters.")]
+        public string UserID { get; set; }
+
+        [Required]
+        [Display(Name = "Course ID")]
+        public int CourseID { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+        public System.DateTime DateCompleted { get; set; }
+    }
+
+    [MetadataType(typeof(CourseCompletionsMetadata))]
+    public partial class CourseCompletions { }
+    #endregion
+
+    #region Courses
+    public class CoursesMetadata
+    {
+        [Required]
+        [Display(Name = "Course")]
+        [StringLength(200, ErrorMessage = "*Course Name must not exceed 200 characters.")]
+        public string CourseName { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(500, ErrorMessage = "*Description must not exceed 500 characters.")]
+        public string CourseDescription { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetadata))]
+    public partial class Courses { }
+    #endregion
+
+    #region Lessons
+    public class LessonsMetadata
+    {
+        [Required]
+        [Display(Name = "Lesson")]
+        [StringLength(200, ErrorMessage = "*Lesson title must not exceed 200 characters.")]
+        public string LessonTitle { get; set; }
+
+        [Required]
+        [Display(Name = "Course ID")]
+        public int CourseID { get; set; }
+
+        [StringLength(300, ErrorMessage = "*Introduction must not exceed 300 characters.")]
+        public string Introduction { get; set; }
+
+        public string VideoURL { get; set; }
+
+        public string PdfFilename { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }    
+
+    [MetadataType(typeof(LessonsMetadata))]
+    public partial class Lessons { }
+    #endregion
+
+    #region LessonViews
+    public class LessonViewsMetadata
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        [StringLength(128, ErrorMessage = "*User ID must not exceed 128 characters.")]
+        public string UserID { get; set; }
+
+        [Required]
+        [Display(Name = "Lesson ID")]
+        public int LessonID { get; set; }
+
+        [Required]
+        [Display(Name = "Viewed")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+        public System.DateTime DateViewed { get; set; }
+    }
+
+    [MetadataType(typeof(LessonViewsMetadata))]
+    public partial class LessonViews { }
+    #endregion
+
+    #region UserDetails
+    public class UserDetailsMetadata
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "*First name must not exceed 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "*Last name must not exceed 50 characters.")]
+        public string LastName { get; set; }
+    }
+
+    [MetadataType(typeof(UserDetailsMetadata))]
+    public partial class UserDetails
+    {
+        [Display(Name = "Name")]
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+    #endregion
+}
---
+++ FinalProjectLMS.UI.MVC/Controllers/LessonViewsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProjectLMS.DATA.EF;
+
+namespace FinalProjectLMS.UI.MVC.Controllers
+{
+    public class LessonViewsController : Controller
+    {
+        private FinalProjectLMSEntities db = new FinalProjectLMSEntities();
+
+        // GET: LessonViews
+        public ActionResult Index()
+        {
+            var lessonViews1 = db.LessonViews1.Include(l => l.Lesson).Include(l => l.UserDetail);
+            return View(lessonViews1.ToList());
+        }
+
+        // GET: LessonViews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LessonViews lessonViews = db.LessonViews1.Find(id);
+            if (lessonViews == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lessonViews);
+        }
+
+        // GET: LessonViews/Create
+        public ActionResult Create()
+        {
+            ViewBag.LessonID = new SelectList(db.Lessons1, "LessonID", "LessonTitle");
+            ViewBag.UserID = new SelectList(db.UserDetails1, "UserID", "FirstName");
+            return View();
+        }
+
+        // POST: LessonViews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LessonViewsID,UserID,LessonID,DateViewed")] LessonViews lessonViews)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LessonViews1.Add(lessonViews);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LessonID = new SelectList(db.Lessons1, "LessonID", "LessonTitle", lessonViews.LessonID);
+            ViewBag.UserID = new SelectList(db.UserDetails1, "UserID", "FirstName", lessonViews.UserID);
+            return View(lessonViews);
+        }
+
+        // GET: LessonViews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LessonViews lessonViews = db.LessonViews1.Find(id);
+            if (lessonViews == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LessonID = new SelectList(db.Lessons1, "LessonID", "LessonTitle", lessonViews.LessonID);
+            ViewBag.UserID = new SelectList(db.UserDetails1, "UserID", "FirstName", lessonViews.UserID);
+            return View(lessonViews);
+        }
+
+        // POST: LessonViews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LessonViewsID,UserID,LessonID,DateViewed")] LessonViews lessonViews)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lessonViews).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LessonID = new SelectList(db.Lessons1, "LessonID", "LessonTitle", lessonViews.LessonID);
+            ViewBag.UserID = new SelectList(db.UserDetails1, "UserID", "FirstName", lessonViews.UserID);
+            return View(lessonViews);
+        }
+
+        // GET: LessonViews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LessonViews lessonViews = db.LessonViews1.Find(id);
+            if (lessonViews == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lessonViews);
+        }
+
+        // POST: LessonViews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LessonViews lessonViews = db.LessonViews1.Find(id);
+            db.LessonViews1.Remove(lessonViews);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
